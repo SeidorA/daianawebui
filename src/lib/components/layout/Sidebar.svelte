95ee3a69684cdab29a7fcf97c8d7a2039ee902dc
Aldemar Garcia <agarcia@seidoranalytics.com>
2025-09-24 09:47:19 -0500

    refactor: comment out new chat link in Sidebar component

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -1063,14 +1063,19 @@
 					/>
 				</a>
 
+<<<<<<< HEAD
 				<a href="/" class="flex flex-1 px-0.5" on:click={newChatHandler}>
 					<div
 						id="sidebar-webui-name"
 						class=" self-center font-medium text-gray-850 dark:text-white font-primary"
 					>
+=======
+				<!-- <a href="/" class="flex flex-1 px-1.5" on:click={newChatHandler}>
+					<div class=" self-center font-medium text-gray-850 dark:text-white font-primary">
+>>>>>>> 952c7f95b (refactor: comment out new chat link in Sidebar component)
 						{$WEBUI_NAME}
 					</div>
-				</a>
+				</a> -->
 				<Tooltip
 					content={$showSidebar ? $i18n.t('Close Sidebar') : $i18n.t('Open Sidebar')}
 					placement="bottom"
